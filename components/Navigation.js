330ab718057187e947b9116fd6dea6a092381575
bhuvneshnstack <bhuvnesh.nstack@gmail.com>
2021-10-18 12:09:18 +0530

    first commit

---
+++ components/Navigation.js
@@ -0,0 +1,13 @@
+export const Navigation = () => {
+    return (
+        <>
+        
+        <div>
+            <h1>Hello world</h1>
+            <p>hello this is a next js app.</p>
+        </div>
+        
+        </>
+    )
+}
+
