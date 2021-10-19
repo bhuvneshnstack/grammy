adee02c62210f6c1522a12a2f44a93f94bf27aee
bhuvneshnstack <bhuvnesh.nstack@gmail.com>
2021-10-19 19:01:09 +0530

    Adding Modal

---
+++ components/Modal.js
@@ -0,0 +1,30 @@
+import Link from "next/dist/client/link"
+function modal() {
+    return (
+        <div>
+            <Link href="/RecordingAcademy">
+                <a>Recording Academy</a>
+            </Link>
+            <Link href="">
+                <a>GRAMMYs</a>
+            </Link>
+            <Link href="">
+                <a>Membership</a>
+            </Link>
+            <Link href="">
+                <a>Advocacy</a>
+            </Link>
+            <Link href="">
+                <a>MusiCares</a>
+            </Link>
+            <Link href="">
+                <a>GRAMMY Museum</a>
+            </Link>
+            <Link href="">
+                <a>Latin GRAMMYs</a>
+            </Link>
+        </div>
+    )
+}
+
+export default modal
